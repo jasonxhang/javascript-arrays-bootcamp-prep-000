e1735e0611b665bf603167851c619e9fe679cab0
Jason Hang <jasonxhang@gmail.com>
2018-06-10 16:15:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"];
+
+function addElementToBeginningOfArray(x, y) {
+  return [y, ...x]
+}
+
+function destructivelyAddElementToBeginningOfArray(x, y) {
+  var z = x.unshift(y);
+  return z;
+}
